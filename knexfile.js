44c046aeea3fd9898e97fe6689abd439197f612f
Jacob <jengopockets@gmail.com>
2021-04-19 23:44:57 -0500

    hit

--- knexfile.js
+++ knexfile.js
@@ -24,6 +24,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: {
       directory:  './database/migrations',
     },
